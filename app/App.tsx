97507ff3b7c7ad736fc04f6a934d1d04c7ce8c1a
CarlosPerez93 <carlos.perez3@outlook.es>
2025-04-15 17:08:44 +0200

    refactor(App): add StatusBar

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,6 @@
 import { useFonts } from 'expo-font'
 import { I18nextProvider } from 'react-i18next'
-import { NativeBaseProvider } from 'native-base'
+import { NativeBaseProvider, StatusBar, useColorMode } from 'native-base'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import i18n from './i18n/config'
@@ -11,7 +11,17 @@ import NavigationDrawer from './components/NavigationDrawer'
 
 export default function App() {
     const [loaded] = useFonts({ Exo, Poppins })
+    const { colorMode } = useColorMode()
 
+    const bg =
+        colorMode === 'light'
+            ? customTheme.colors.dark.bg
+            : customTheme.colors.light.bg
+
+    const txt =
+        colorMode === 'light'
+            ? customTheme.colors.dark.bg
+            : customTheme.colors.light.bg
     return loaded ? (
         <I18nextProvider i18n={i18n}>
             <SafeAreaProvider>
@@ -20,6 +30,7 @@ export default function App() {
                     colorModeManager={colorModeManager}
                 >
                     <NavigationDrawer />
+                    <StatusBar backgroundColor={bg} />
                 </NativeBaseProvider>
             </SafeAreaProvider>
         </I18nextProvider>
